2e5956a58d1c0745681c078532eece769f2a2a4e
Stevan Little <stevan.little@iinteractive.com>
2012-10-26 17:07:17 -0400

    use the Panel object

--- example/app.ts
+++ example/app.ts
@@ -7,52 +7,39 @@ class Example {
     public persons        : Kart.Model.Resource[] = [];
     public current_person : Kart.Model.Resource;
 
-    public actions = {
-        save : new Kart.Binding.Action ({
-            element       : $('#save'),
-            event_type    : 'click',
-            target_action : 'save_person'
-        }),
-        cancel : new Kart.Binding.Action ({
-            element       : $('#cancel'),
-            event_type    : 'click',
-            target_action : 'cancel_edit'
-        })
-    };
-
-    public outlets = {
-        first_name : new Kart.Binding.Outlet ({
-            element  : $('#first_name'),
-            property : "first_name"
-        }),
-        last_name : new Kart.Binding.Outlet ({
-            element  : $('#last_name'),
-            property : "last_name"
-        })
-    };
+    public panel = new Kart.UI.Panel({
+        outlets : [
+            new Kart.UI.Textbox ({
+                element  : $('#first_name'),
+                property : "first_name"
+            }),
+            new Kart.UI.Textbox ({
+                element  : $('#last_name'),
+                property : "last_name"
+            })
+        ],
+        actions : [
+            new Kart.UI.Button ({
+                element       : $('#save'),
+                event_type    : 'click',
+                target_action : 'save_person'
+            }),
+            new Kart.UI.Button ({
+                element       : $('#cancel'),
+                event_type    : 'click',
+                target_action : 'cancel_edit'
+            })
+        ]
+    });
 
     constructor() {
+        this.panel.set_responder( this );
         this.initialize_new_resource();
-        this.bind_actions( this );
     }
 
     private initialize_new_resource () {
-        this.current_person = this.new_resource();
-        this.bind_outlets( this.current_person );
-    }
-
-    private new_resource () { return new Kart.Model.Resource({}) }
-
-    private bind_outlets ( target ) {
-        for ( var o in this.outlets ) {
-            this.outlets[ o ].set_target( target );
-        }
-    }
-
-    private bind_actions ( target ) {
-        for ( var a in this.actions ) {
-            this.actions[ a ].set_target( target );
-        }
+        this.current_person = new Kart.Model.Resource({});
+        this.panel.set_data_source( this.current_person );
     }
 
     save_person ( e ) {
