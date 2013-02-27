c88acc79a468bb80048df27c84edd3852e04ae79
Shawn M Moore <code@sartak.org>
2013-02-27 15:09:54 -0500

    Extract templates from markup

--- examples/002-templates/controller.ts
+++ examples/002-templates/controller.ts
@@ -4,7 +4,8 @@
 /// <reference path="../../lib/Oryx.ts" />
 
 class OrderController {
-    public order;
+    public order : Order;
+    public templates : any;
 
     public view = Oryx.UI.Panel.inflate({
         outlets : {
@@ -21,13 +22,23 @@ class OrderController {
     });
 
     constructor() {
+        this.templates = {};
+
         this.order = new Order(null, {
             name     : "New Order",
             products : new Products(),
         });
     }
 
+    extract_templates() {
+        this.templates.sku         = Handlebars.compile($("#sku-template").html());
+        this.templates.description = Handlebars.compile($("#description-template").html());
+        this.templates.quantity    = Handlebars.compile($("#quantity-template").html());
+        this.templates.available   = Handlebars.compile($("#available-template").html());
+    }
+
     bind() {
+        this.extract_templates();
         this.view.set_responder( this );
         this.table.set_data_source( this.order.get("products") );
     }
